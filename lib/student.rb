da91746d17324e66736e5056ae6fb2c1bb564d74
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-25 22:37:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,10 +35,6 @@ class Student
       VALUES (?,?)
       SQL
     DB[:conn].execute(sql, self.name, self.grade)
-      sql = <<-SQL
-      SELECT * FROM students
-      WHERE ID = 1
-      SQL
   end
 
 
